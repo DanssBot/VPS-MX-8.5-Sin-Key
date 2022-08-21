a4909b72190ba2576c4c1f46ae23a5aab36801a8
DanssBot <90471487+DanssBot@users.noreply.github.com>
2022-08-20 20:56:46 -0500

    Update Install-Sin-Key.sh

--- Install Sin KEY/Install-Sin-Key.sh
+++ Install Sin KEY/Install-Sin-Key.sh
@@ -3,8 +3,6 @@ clear && clear
 cd $HOME
 RutaBin="/bin"
 apt install net-tools -y &>/dev/null
-echo "nameserver 1.1.1.1" >/etc/resolv.conf
-echo "nameserver 1.0.0.1" >>/etc/resolv.conf
 myip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1)
 myint=$(ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}')
 rm -rf /etc/localtime &>/dev/null
