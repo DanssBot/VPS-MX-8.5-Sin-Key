665c461e3ef0cf0b967f2ea4d38dc0cf12d9672c
DanssBot <90471487+DanssBot@users.noreply.github.com>
2022-09-10 11:22:30 -0500

    Update Install-Sin-Key.sh

--- Install/Install-Sin-Key.sh
+++ Install/Install-Sin-Key.sh
@@ -6,6 +6,7 @@ rm -rf /etc/localtime &>/dev/null
 ln -s /usr/share/zoneinfo/America/Argentina/Tucuman /etc/localtime &>/dev/null
 
 apt install net-tools -y &>/dev/null
+apt install lolcat -y &>/dev/null
 myip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1)
 myint=$(ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}')
 rm -rf /etc/localtime &>/dev/null
@@ -167,7 +168,6 @@ post_reboot() {
 }
 
 install_start() {
-  msg -bar
   msg -bar
   figlet "Actualizar" | lolcat 
   msg -bar
